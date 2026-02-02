a1988ddfd68f58604a1e39b07935bd738208b61e
GauravAthode <gauravathode123@gmail.com>
2026-02-02 21:48:14 +0530

    commit

--- client/src/components/restaurantDashboard/RestaurantMenu.jsx
+++ client/src/components/restaurantDashboard/RestaurantMenu.jsx
@@ -1,24 +1,28 @@
 import React from "react";
+import AddMenuItemModal from "./modals/AddMenuItemModal";
 
 const RestaurantMenu = () => {
+  const [isAddItemModalOpen, setIsAddItemModalOpen] = React.useState(false);
   return (
-    <div className="bg-gray-50 rounded-lg p-6 h-full overflow-y-auto">
-      <div className="bg-white rounded-lg shadow-md p-6 border border-gray-200">
-        <div className="flex justify-between">
+    <>
+      <div className="bg-gray-50 rounded-lg p-6 h-full overflow-y-auto">
+        <div className="bg-white rounded-lg shadow-md p-6 border border-gray-200 flex justify-between">
           <h2 className="text-2xl font-bold text-gray-800 mb-4">
             Menu Management
           </h2>
-          <div>
-            <button className=" bg-(--color-secondary) rounded p-2 font-bold text-white hover:bg-amber-600">Add items +</button>
-          </div>
-        </div>
-        <div className="text-center text-gray-500 py-12">
-          <p className="text-lg">
-            Menu items will be displayed and managed here
-          </p>
+          <button
+            className="px-4 py-2 bg-(--color-secondary) text-white rounded-lg hover:bg-(--color-secondary-hover) transition font-semibold"
+            onClick={() => setIsAddItemModalOpen(true)}
+          >
+            Add Item <b>+</b>
+          </button>
         </div>
       </div>
-    </div>
+
+      {isAddItemModalOpen && (
+        <AddMenuItemModal onClose={() => setIsAddItemModalOpen(false)} />
+      )}
+    </>
   );
 };
 
